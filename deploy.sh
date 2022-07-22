f8100c9d5fc6b5b369082c3e6037e421675d446d
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2022-07-22 23:54:27 +0900

    :zap: prepare

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,28 @@
+#!/bin/bash -eux
+
 function deploy () {
-  server="$(hostname)"
-  basepath="/home/isucon/webapp" # Tobe changed
+  basepath="/home/isucon/webapp" # To Be Changed
+
   for file in "$(find all -type f)"; do
-    cp basepath/$file /${file#*/}
+    cp $basepath/$file /${file#*/}
   done
-  for file in "$(find $server -type f)"; do
-    cp basepath/$file /${file#*/}
+  for file in "$(find $HOSTNAME -type f)"; do
+    cp $basepath/$file /${file#*/}
   done
+
+  (cd /home/isucon/webapp/go && go build -o app)
+
+  # sudo systemctl restart nginx
+  # sudo systemctl restart mysql
+  # sudo systemctl restart isucon.go
+
+  sudo sysctl -p /etc/sysctl.d/100-isucon.conf
+  
+  # refresh
+  source $basepath/deploy.sh
+}
+
+function sync () {
+  git fetch && \
+  git reset --hard origin/$1 # <username>/<branch_name>
 }
