d2b403076f8076c1baa7210b1b7f0c6c1f6ce19a
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2023-11-24 22:56:37 +0900

    alter

--- commands.sh
+++ commands.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -eux
 
-function deploy () {
+# ホームディレクトリの位置
+# ビルド先
+# 起動しているミドルウェア
+
+function deploy() {
   # WARN: .gitkeep is also added to /.gitkeep
-  for src in `find /home/isucon/webapp/all -type f`; do
+  for src in $(find /home/isucon/webapp/all -type f); do
     dst="$(echo $src | sed "s/\/home\/isucon\/webapp\/all//")"
     sudo cp $src $dst
   done
-  for src in `find /home/isucon/webapp/$HOSTNAME -type f`; do
+  for src in $(find /home/isucon/webapp/$HOSTNAME -type f); do
     dst=$(echo $src | sed "s/\/home\/isucon\/webapp\/$HOSTNAME//")
     sudo cp $src $dst
   done
@@ -22,14 +26,14 @@ function deploy () {
   sudo sysctl -p /etc/sysctl.d/99-isucon.conf
 }
 
-function sync () {
-  git fetch && \
-  git reset --hard origin/$1 # <branch_name>
+function sync() {
+  git fetch &&
+    git reset --hard origin/$1 # <branch_name>
   source /home/isucon/webapp/commands.sh
 }
 
-function gather () {
-  for file in `cat /home/isucon/webapp/target.txt`; do
+function gather() {
+  for file in $(cat /home/isucon/webapp/target.txt); do
     mkdir -p /home/isucon/webapp/all$(dirname $file)
     sudo cp $file /home/isucon/webapp/all$file
   done
